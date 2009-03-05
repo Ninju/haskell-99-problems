2a2f1d7cbb68dda10628243672f5e96d4a2228ff
Alex <alex.watt@me.com>
2009-03-05 13:14:35 +0000

    Added solution for Problem 9

--- problems1-10.hs
+++ problems1-10.hs
@@ -1,3 +1,13 @@
 module Main where
 
 -- Solutions for Problems 1-10
+
+
+-- Problem 9
+
+groupDuplicates :: Eq a => [a] -> [[a]]
+groupDuplicates []     = []
+groupDuplicates (x:xs) = (x:duplicates) : groupDuplicates rest
+                         where
+                         (duplicates, rest) = span (x==) xs
+
