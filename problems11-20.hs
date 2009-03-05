bfec0451acb2526dadeeedd540e7f19f828c5ec2
Alex Watt <alex.watt@me.com>
2009-03-05 22:11:54 +0000

    Added temporary solution for Problem 11

--- problems11-20.hs
+++ problems11-20.hs
@@ -1,2 +1,13 @@
 module Main where
+import Data.List (group)
 
+-- Problem 11
+
+data Element a = Element a | EncodedElement Int a deriving Show
+
+encode' :: Eq a => [a] -> [Element a]
+encode' = map encodeElem' . group
+          where
+          encodeElem' ys = case ys of
+                            [y] -> Element y
+                            _   -> EncodedElement (length ys) (head ys)
