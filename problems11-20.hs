d6db656dc3493e2bb12fcae55d152f96b5a18b08
Alex Watt <alex.watt@me.com>
2009-03-09 22:54:37 +0000

    Added solution for Problem 16

--- problems11-20.hs
+++ problems11-20.hs
@@ -30,3 +30,9 @@ dupli = concatMap (replicate 2)
 
 repli :: Int -> [a] -> [a]
 repli = concatMap . replicate
+
+-- Problem 16
+
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery xs n = take (n - 1) xs ++ dropEvery (drop n xs) n
