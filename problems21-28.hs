d010a8576b549a480a00b53b18314d77d19c9f51
Alex <alex.watt@me.com>
2009-03-10 13:49:23 +0000

    Added solution for Problem 21

--- problems21-28.hs
+++ problems21-28.hs
@@ -1,2 +1,8 @@
 module Main where
 
+-- Problem 21
+
+insertAt :: Int -> a -> [a] -> [a]
+insertAt n x xs | n < 1     = xs
+                | otherwise = let (ys, zs) = splitAt (n - 1) xs
+                              in ys ++ (x:zs)
