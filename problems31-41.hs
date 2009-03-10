381179bb1b4d0ddb1c1a5597246cf937bc0ade1b
Alex <alex.watt@me.com>
2009-03-10 14:48:34 +0000

    Swapped find for elem

--- problems31-41.hs
+++ problems31-41.hs
@@ -9,6 +9,4 @@ primes = sieve [2..]
          sieve (p:xs) = p : sieve (filter (not . (== 0) . (`mod` p)) xs)
 
 isPrime :: Integer -> Bool
-isPrime n = case find (== n) $ takeWhile (<= n) primes of
-              Nothing -> False
-              Just _  -> True
+isPrime n = elem n $ takeWhile (<= n) primes
