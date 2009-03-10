48d0b50127b240357267435098824071f3a9b047
Alex <alex.watt@me.com>
2009-03-10 14:46:22 +0000

    Added solution for Problem 31

--- problems31-41.hs
+++ problems31-41.hs
@@ -1,2 +1,14 @@
 module Main where
+import Data.List (find)
 
+-- Problem 31
+
+primes :: [Integer]
+primes = sieve [2..]
+         where
+         sieve (p:xs) = p : sieve (filter (not . (== 0) . (`mod` p)) xs)
+
+isPrime :: Integer -> Bool
+isPrime n = case find (== n) $ takeWhile (<= n) primes of
+              Nothing -> False
+              Just _  -> True
