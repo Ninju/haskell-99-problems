109fa9146d42da9714532810bdf5fca41fa38f34
Alex <alex.watt@me.com>
2009-03-10 13:58:09 +0000

    Added solution for Problem 22

--- problems21-28.hs
+++ problems21-28.hs
@@ -6,3 +6,7 @@ insertAt :: Int -> a -> [a] -> [a]
 insertAt n x xs | n < 1     = xs
                 | otherwise = let (ys, zs) = splitAt (n - 1) xs
                               in ys ++ (x:zs)
+
+range :: Int -> Int -> [Int]
+range x y | x > y     = []
+          | otherwise = x : range (x + 1) y
