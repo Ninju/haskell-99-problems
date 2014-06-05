aef5af27e9aa2d6effd0bcf07cc6a497a0011ae8
Alex <el.zingo@gmail.com>
2014-06-05 12:40:39 +0300

    Added Problem 26 -- 'combinations'

--- problems21-28.hs
+++ problems21-28.hs
@@ -42,3 +42,11 @@ diff_select n maxV = rnd_select [1..maxV] n
 
 rnd_permu :: [a] -> IO [a]
 rnd_permu xs = return . concat =<< rnd_select (permutations xs) 1
+
+-- Problem 26
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 xs     = []
+combinations 1 xs     = map (:[]) xs
+combinations n []     = []
+combinations n (x:xs) = (map (x:) $ combinations (n - 1) xs) ++ combinations n xs
