705c8628d0daf8527bf7869f2dbaaf53656a484d
Alex <el.zingo@gmail.com>
2016-06-19 10:03:31 +0300

    Addd goldbachList' function for Problem 41 (complete)

--- problems31-41.hs
+++ problems31-41.hs
@@ -78,8 +78,15 @@ goldbach n =
 
 -- Problem 41
 
+andP f g (x,y) = f x && g y
+
 goldbachList :: Integer -> Integer -> [(Integer, Integer)]
 goldbachList lower upper 
   | (lower `mod` 2) /= 0 = goldbachList (lower + 1) upper
   | lower > upper = []
   | otherwise = goldbach lower : goldbachList (lower + 2) upper
+
+goldbachList' lower upper lowerPrime =
+  let biggerThanLowerPrime = (>= lowerPrime)
+  in
+    filter (andP biggerThanLowerPrime biggerThanLowerPrime) $ goldbachList lower upper
