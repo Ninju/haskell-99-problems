b439b8d7c18f5ef47ab7187198854770b9284f59
Alex <el.zingo@gmail.com>
2016-06-22 11:03:20 +0300

    Add Problem 48

--- problems46-50.hs
+++ problems46-50.hs
@@ -1,5 +1,6 @@
 module Main where
 import Prelude hiding (and, or)
+import Data.List (intercalate)
 
 -- Problem 46
 
@@ -31,3 +32,29 @@ table logicF = mapM (putStrLn . showTableRow . buildRow logicF) booleanCombos
                where
                buildRow f (a,b) = (a, b, f a b)
                showTableRow (a,b,c) = showBool a ++ " " ++ showBool b ++ " " ++ showBool c
+
+-- Problem 48
+
+choose :: [[a]] -> [[a]]
+choose []       = []
+choose (xs:[])  = map (:[]) xs
+choose (xs:xss) =
+  let restOfChoices = choose xss
+  in
+    do subChoice <- restOfChoices
+       x         <- xs
+       [x:subChoice]
+
+copy n xs = map (const xs) [1..n]
+
+generateCombinations :: Int -> [a] -> [[a]]
+generateCombinations n values =
+  let allChoices = copy n values
+  in
+    choose allChoices
+
+tablen n logicF =
+  mapM (putStrLn . showTableRow . buildRow logicF) (generateCombinations 3 [True, False])
+  where
+  buildRow f = \xs -> xs ++ [f xs]
+  showTableRow xs = intercalate " " $ map showBool xs
